bebe61f36983c379f115809dd3f07746f8bb1a47
krutika jadhav <krutikajadhav012@gmail.com>
2024-10-29 12:01:47 +0530

    new code

---
+++ interior.php
@@ -0,0 +1,42 @@
+<?php 
+ini_set('session.cache_limiter','public');
+session_cache_limiter(false);
+session_start();
+include("config.php");
+								
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- <link rel="stylesheet" href="css/knowabout.css"> -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/homeloan.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Italiana&family=Lexend&display=swap" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+
+<!-- navbar -->
+
+<?php include("include/header.php");?>
+
+ <!-- end navbar -->
+
+
+  <!-- banner -->
+  <section id="banner-main">
+    <div class="banner1" >
+        <div class="banner-content">
+            <h2>Home Loan</h2>
+            <p><a href="#" class="banner-bread">Home</a> &gt; <a href="#" class="banner-bread">Home Loan</a></p>
+        </div>
+    </div>
+</section>
